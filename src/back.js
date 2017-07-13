ced3c4c28b9a0952591e9951a2437f2f1c3d278f
VladKholod <vlad-bond@mail.ru>
2017-07-13 14:49:58 +0300

    Addede all

--- src/back.js
+++ src/back.js
@@ -1,3 +1,9 @@
 /**
  * Created by student01 on 13.07.2017.
  */
+jQuery(document).ready(function () {
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'config.json', function() {
+    console.log('callback - particles-js config loaded');
+});
+});
--- src/diagram.js
+++ src/diagram.js
@@ -1,7 +1,6 @@
 /**
  * Created by student01 on 27.06.2017.
  */
-(function(){
 
     var app = angular.module('graphApp',[]);
 
@@ -10,117 +9,41 @@
         $scope.show;
         $scope.infoElemet = document.createElement("div");
         $scope.infoElemet.className = "info";
+        $scope.infoElemet.style.zIndex = 3;
         $scope.infoElemet.style.display="none";
         document.body.appendChild($scope.infoElemet);
         $scope.isNowTarget=null;
         $scope.width = 600;
         $scope.height = 350;
-        $scope.yAxis = 'Курс, руб';
-        $scope.xAxis = 'Февраль 2016';
+        $scope.yAxis = 'Курс, BYN по отношению к USD';
+        $scope.xAxis = 'Февраль 2017';
+        $scope.max = 0;
 
-        $http.get('example.json').then(function(res) {
-            $scope.countries = res.data;
-        });
 
-        $scope.data = [
-            {
-                "day": 1,
-                "course":25.55
-            },
-            {
-                "day": 2,
-                "course":25.44
-            },{
-                "day": 3,
-                "course":25.87
-            },{
-                "day": 4,
-                "course":25.69
-            },{
-                "day": 5,
-                "course":25.87
-            },{
-                "day": 6,
-                "course":25.33
-            },{
-                "day": 7,
-                "course":25.11
-            },{
-                "day": 8,
-                "course":25.91
-            },{
-                "day": 9,
-                "course":25.87
-            },{
-                "day": 10,
-                "course":25.94
-            },{
-                "day": 11,
-                "course":25.94
-            },{
-                "day": 12,
-                "course":26.08
-            },{
-                "day": 13,
-                "course":26.08
-            },{
-                "day": 14,
-                "course":26.09
-            },{
-                "day": 15,
-                "course":26.14
-            },{
-                "day": 16,
-                "course":26.84
-            },{
-                "day": 17,
-                "course":27.01
-            },{
-                "day": 18,
-                "course":26.72
-            },{
-                "day": 19,
-                "course":26.46
-            },{
-                "day": 20,
-                "course":26.46
-            },{
-                "day": 21,
-                "course":26.44
-            },{
-                "day": 22,
-                "course":26.85
-            },{
-                "day": 23,
-                "course":27.01
-            },{
-                "day": 24,
-                "course":27.23
-            },{
-                "day": 25,
-                "course":27.21
-            },{
-                "day": 26,
-                "course":27.24
-            },{
-                "day": 27,
-                "course":27.24
-            },
-            {
-                "day":28,
-                "course":27.46
-            }
-        ];
+        $http.get("http://www.nbrb.by/API/ExRates/Rates/Dynamics/145?startDate=2017-2-1&endDate=2017-2-28")
+            .then(function(response) {
+                $scope.details = response.data;
+                $scope.count = $scope.details.length;
+                console.log($scope.count);
+
+                let arrLength = $scope.count;
+                console.log(arrLength);
+                for (let i = 0; i < arrLength; i++) {
+                    if ($scope.details[i].Cur_OfficialRate > $scope.max)
+                        $scope.max = $scope.details[i].Cur_OfficialRate;
+                }
+            });
+
+
 
        $scope.targetNow = (event)=>{
            let target = event.target;
-           console.log(target.getAttribute("data-info"));
-           if(target.tagName == "LI") {
+           if(target.tagName == "rect" ) {
                let info = target.getAttribute("data-info");
                if(!info)return;
                $scope.infoElemet.innerHTML = info;
                let coords = target.getBoundingClientRect();
-               let left = coords.left + (target.offsetWidth - $scope.infoElemet.offsetWidth) / 2;
+               let left = coords.left + (16 - $scope.infoElemet.offsetWidth) / 2;
                if (left < 0) left = 0;
                let top = coords.top - $scope.infoElemet.offsetHeight - 5;
                if (top < 0) {
@@ -132,6 +55,10 @@
                $scope.infoElemet.style.display = "block";
            }else return;
        };
+        $scope.trash = (event)=>{
+            let target = event.target;
+            alert(target.getAttribute("data-info"));
+        };
 
         $scope.reset = (event)=>{
             let target = event.target;
@@ -141,15 +68,5 @@
             }
         };
 
-        $scope.max = 0;
-
-        var arrLength = $scope.data.length;
-        for (var i = 0; i < arrLength; i++) {
-            if ($scope.data[i].course > $scope.max)
-                $scope.max = $scope.data[i].course;
-        }
-
-
     });
 
-})();
